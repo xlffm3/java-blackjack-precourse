6cbe643ebaf813609d71a2e84158428fb9932f4e
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-12-18 19:12:36 +0900

    feat: Dealer 1장 뽑는 기능 구현

--- src/main/java/blackjack/domain/user/Dealer.java
+++ src/main/java/blackjack/domain/user/Dealer.java
@@ -10,16 +10,22 @@ import java.util.List;
 
 public class Dealer {
     private static final int ONE_CARD_INDEX = 0;
+    private static final int MAXIMUM_REDRAW_SCORE_TOTAL = 16;
 
     private final Cards cards = new Cards(new ArrayList<>());
-
-    public void addCard(Card card) {
-        cards.addCard(card);
-    }
 
     public void drawDefaultCards(CardDeque cardDeque) {
         List<Card> defaultCards = cardDeque.drawDefaultCards();
         defaultCards.forEach(this::addCard);
+    }
+
+    private void addCard(Card card) {
+        cards.addCard(card);
+    }
+
+    public void drawCard(CardDeque cardDeque) {
+        Card card = cardDeque.drawCard();
+        cards.addCard(card);
     }
 
     public List<CardDto> getCardDtos() {
@@ -28,5 +34,9 @@ public class Dealer {
 
     public CardDto getOneCardDto() {
         return getCardDtos().get(ONE_CARD_INDEX);
+    }
+
+    public boolean isAbleToDrawCard() {
+        return cards.getScoreTotal() <= MAXIMUM_REDRAW_SCORE_TOTAL;
     }
 }
