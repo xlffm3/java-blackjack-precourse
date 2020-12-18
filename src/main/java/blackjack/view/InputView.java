c9b51a69f2cecaec90603d83233d648c783b138b
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-12-18 14:42:15 +0900

    feat: InputView 각 플레이어별 배팅 금액 입력 기능 구현

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -1,5 +1,6 @@
 package blackjack.view;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -7,6 +8,7 @@ import java.util.stream.Collectors;
 
 public class InputView {
     private static final String INPUT_PLAYER_NAMES_NOTICE = "게임에 참여할 사람의 이릅을 입력하세요.(쉼표 기준으로 분리)";
+    private static final String INPUT_BETTING_MONEY_NOTICE = "%s의 배팅 금액은?\n";
     private static final String COMMA_DELIMITER = ",";
     private static final int SPLIT_RESULT_THRESHOLD = -1;
 
@@ -22,5 +24,17 @@ public class InputView {
                 .split(COMMA_DELIMITER, SPLIT_RESULT_THRESHOLD);
         return Arrays.stream(playerNames)
                 .collect(Collectors.toList());
+    }
+
+    public List<Long> inputBettingMoney(List<String> playerNames) {
+        List<Long> bettingMoney = new ArrayList<>();
+        playerNames.forEach(playerName -> inputEachBettingMoney(playerName, bettingMoney));
+        return bettingMoney;
+    }
+
+    private void inputEachBettingMoney(String playerName, List<Long> bettingMoney) {
+        System.out.printf(INPUT_BETTING_MONEY_NOTICE, playerName);
+        long playerBettingMoney = Long.parseLong(scanner.nextLine());
+        bettingMoney.add(playerBettingMoney);
     }
 }
