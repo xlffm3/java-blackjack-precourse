4fe8ad78c24f1ad8594cdde8ddb385231a8a4939
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-12-18 23:25:48 +0900

    feat: InputView 카드 추가 발급 선택 기능 구현

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -8,7 +8,8 @@ import java.util.stream.Collectors;
 
 public class InputView {
     private static final String INPUT_PLAYER_NAMES_NOTICE = "게임에 참여할 사람의 이릅을 입력하세요.(쉼표 기준으로 분리)";
-    private static final String INPUT_BETTING_MONEY_NOTICE = "%s의 배팅 금액은?\n";
+    private static final String INPUT_BETTING_MONEY_NOTICE = "\n%s의 배팅 금액은?\n";
+    private static final String INPUT_CHOICE_TO_DRAW_CARD_NOTICE = "\n%s는 한 장의 카드를 더 받겠습니까?\n";
     private static final String COMMA_DELIMITER = ",";
     private static final int SPLIT_RESULT_THRESHOLD = -1;
 
@@ -36,5 +37,10 @@ public class InputView {
         System.out.printf(INPUT_BETTING_MONEY_NOTICE, playerName);
         long playerBettingMoney = Long.parseLong(scanner.nextLine());
         bettingMoneys.add(playerBettingMoney);
+    }
+
+    public String inputChoiceToDrawCard(String name) {
+        System.out.printf(INPUT_CHOICE_TO_DRAW_CARD_NOTICE, name);
+        return scanner.nextLine();
     }
 }
