23d5788981c052253d1a13a51ac31e603d56b09e
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-12-18 23:17:07 +0900

    feat: Dealer 수익률 계산 기능 구현

--- src/main/java/blackjack/domain/user/Dealer.java
+++ src/main/java/blackjack/domain/user/Dealer.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 public class Dealer {
     private static final int ONE_CARD_INDEX = 0;
+    private static final int PROFIT_ADJUSTMENT_NUMBER = -1;
     private static final int MAXIMUM_REDRAW_SCORE_TOTAL = 16;
 
     private final Cards cards = new Cards(new ArrayList<>());
@@ -37,6 +38,29 @@ public class Dealer {
     }
 
     public boolean isAbleToDrawCard() {
-        return cards.getScore() <= MAXIMUM_REDRAW_SCORE_TOTAL;
+        return cards.hasScoreLessOrEqual(MAXIMUM_REDRAW_SCORE_TOTAL);
+    }
+
+    public boolean isBlackJack() {
+        return cards.isBlackJack();
+    }
+
+    public boolean isBust() {
+        return cards.isBust();
+    }
+
+    public boolean hasScoreGreater(int playerScore) {
+        return cards.hasScoreGreater(playerScore);
+    }
+
+    public boolean hasScoreEqual(int playerScore) {
+        return cards.hasScoreEqual(playerScore);
+    }
+
+    public int calculateProfit(Players players) {
+        return players.calculateProfits(this)
+                .stream()
+                .mapToInt(playerProfit -> playerProfit)
+                .sum() * PROFIT_ADJUSTMENT_NUMBER;
     }
 }
